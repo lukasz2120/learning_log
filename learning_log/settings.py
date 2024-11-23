ed02eb33581c516a4cdf774ff457b424a150bd56
Lukasz Polczyk <lukasz.polczyk@microsoft.wsei.edu.pl>
2024-11-23 19:29:27 +0100

    Definiowanie oraz aktywacja modeli | Tworzenie superusera

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,12 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    #Moje aplikacje
+    'learning_logs',
+
+    #Domyślne aplikacje
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
