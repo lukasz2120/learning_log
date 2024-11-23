d37f4439d581ad36a727ad3564e599b1c02efd6c
Lukasz Polczyk <lukasz.polczyk@microsoft.wsei.edu.pl>
2024-11-23 20:20:57 +0100

    Dodanie modulu entries

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,3 +8,16 @@ class Topic(models.Model):
     def __str__(self):
         """Zwraca reprezentacje modelu w postaci ciągu tekstowego"""
         return self.text
+    
+class Entry(models.Model):
+    """Informacje, wpisy odnośnie konkretnego tematu"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        verbose_name_plural = 'entries'
+
+        def __str__(self):
+            """Zwaraca reprezentacje modelu w postaci ciągu tesktowego"""
+            return f"{self.text[:50]}...",
