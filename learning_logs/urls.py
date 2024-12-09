334881f56e00f14841e3da7f3c367eb3f1ce0541
Lukasz Polczyk <lukasz.polczyk@microsoft.wsei.edu.pl>
2024-12-09 16:03:34 +0100

    Stworzenie nowych szablownów, base.html,topics.html,topic.html oraz index.html wraz z konfiguracja

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,9 +3,13 @@
 from django.urls import path
 from . import views
 
-app_name = 'leraning_logs'
+app_name = 'learning_logs'
 urlpatterns = [
     #strona głowna
-    path('', views.index,name='index')
+    path('', views.index,name='index'),
+    #Wyświetlanie wszystkich tematów
+    path('topics/', views.topics,name='topics'),
+    #Strona szczegółowa dotycząca poszczególnego tematu
+    path('topics/(<int:topic_id>)/',views.topic,name='topic')
 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
     """Strona głowna dla aplikacji Learning log."""
     return render(request, 'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\index.html')
+
+def topics(request):
+    """Wyświetlanie wszystkich tematów"""
+    topics = Topic.objects.order_by("date_added")
+    context = {'topics': topics}
+    return render(request, 'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\topics.html', context)
+
+def topic(request,topic_id):
+    """Wyświetla pojedyńczy temat i wszystkie powiązane z nim wpisy"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request,'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\topics.html',context)
