6df509182f127a556b7deeaceca06004c7b0574c
Lukasz Polczyk <lukasz.polczyk@microsoft.wsei.edu.pl>
2024-12-12 13:41:11 +0100

    Dodanie funkcjonalnosci dodawania oraz edytowania Tematów oraz wpisów

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,6 +10,13 @@ urlpatterns = [
     #Wyświetlanie wszystkich tematów
     path('topics/', views.topics,name='topics'),
     #Strona szczegółowa dotycząca poszczególnego tematu
-    path('topics/(<int:topic_id>)/',views.topic,name='topic')
+    path('topics/(<int:topic_id>)/',views.topic,name='topic'),
+    #Dodawanie nowego tematu
+    path('new_topic/',views.new_topic,name='new_topic'),
+    #Dodawanie nowego wpisu
+    path('new_entry/(<int:topic_id>)/',views.new_entry,name='new_entry'),
+    #Strona przeznaczona do edycji wpisu
+    path('edit_entry/<int:entry_id>/', views.edit_entry,name="edit_entry")
+    
 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import render,redirect
+
+from .models import Topic,Entry
+from .forms import TopicForm,EntryForm
 
 def index(request):
     """Strona głowna dla aplikacji Learning log."""
@@ -16,4 +18,61 @@ def topic(request,topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
-    return render(request,'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\topics.html',context)
+    return render(request,'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\topic.html',context)
+
+def new_topic(request):
+    """Dodaj nowy temat"""
+    if request.method != "POST":
+        #Nieprzekazano żadnych danych, należy utworzyć pusty formularz.
+        form = TopicForm()
+    else:
+        #Przekazano dane i należy je przetworzyć
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    
+    #Wyświetlanie pustego formularza
+    context = {'form': form}
+    return render(request,'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Dodanie nowego wpisu dla konkretnego tematu"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        #Nie przekazano danych, należy utworzyć pusty formularz
+        form = EntryForm()
+    else:
+        #Przekazano dane za pomocą żądania POST
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id = topic_id)
+        
+    #Wyświetlanie pustego formularza
+    context = {'topic': topic, 'form': form}
+    return render(request, 'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\new_entry.html',context)
+
+
+def edit_entry(request,entry_id):
+    """Edycja insteniejącego wpisu"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #Nie przekazano danych, należy utworzyć pusty formularz
+        form = EntryForm(instance=entry)
+    else:
+        #Przekazano dane za pomocą żądania POST
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic.id==topic.id)
+    
+    context = {'entry': entry, 'topic': topic,'form': form}
+    return render(request, 'D:\\projects\\learning_log\\learning_logs\\templates\\learning_logs\\new_entry.html', context)
+
+        
