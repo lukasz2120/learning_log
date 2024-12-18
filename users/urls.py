740c23852815d06ee54d21247d4ef57c70e6ec50
Lukasz Polczyk <lukasz.polczyk@microsoft.wsei.edu.pl>
2024-12-18 12:11:09 +0100

    Doprecyzowanie widoków oraz szablonów do zarzadzania uytkownikami

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 """Definiuje wzorce adresów URL dla apliakcji users"""
 
 from django.urls import path,include
+from . import views
 
 app_name = 'users'
 urlpatterns = [
 #Dołączanie domyślnych adresówu URL uwierzytelniania
 path('', include('django.contrib.auth.urls')),
+#Strona rejestracji
+path('register/',views.register,name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,2 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
+def register(request):
+    """Rejestracja nowego użytkownika"""
+    if request.method != "POST":
+        #wyświetlanie pustego formularza
+        form = UserCreationForm()
+    else:
+        #Przetwarzanie wypełnionego formularza
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid:
+            new_user = form.save()
+            #Zalogowanie użytkownika, a następnie przekierowanie na 
+            #główna strone
+            login(request,new_user)
+            return redirect('learning_logs:index')
+        
+    #Wyświetlenie pustego formularza
+    context = {'form': form}
+    return render(request, 'registration/register.html',context)
